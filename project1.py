f043aec596f73acbc72b198f7bf443ae3eff2c62
Besnik Cani <besnik.cani@ait.ac.at>
2023-03-29 13:01:41 +0200

    group tokens by category

--- project1.py
+++ project1.py
@@ -2,7 +2,7 @@ import json
 import re
 from concurrent import futures
 
-data = []
+data = {}
 stopWordsHash = {}
 with open('stopwords.txt', 'r') as file:
     for word in file.read().split("\n"):
@@ -13,13 +13,21 @@ def tokenizeReview(review):
     review = json.loads(review)
     # the regex can be improved to reject single character words
     tokens = re.findall(r'\b[^\d\W]+\b|[()[]{}.!?,;:+=-_`~#@&*%€$§\/]^', review["reviewText"])
-    return [token.lower() for token in tokens if token.lower() not in stopWordsHash and len(token) > 1]
+    return {
+        "category": review['category'],
+        "tokens": [token.lower() for token in tokens if token.lower() not in stopWordsHash and len(token) > 1]
+    }
 
 #Parallelization of the tokenization and filtering
 with open('reviews_devset.json', 'r') as reviews:
     with futures.ThreadPoolExecutor(max_workers=1000) as executor:
         fs = {executor.submit(tokenizeReview, review): review for review in reviews}
         for f in futures.as_completed(fs):
-            data.append(f.result())
+            res = f.result()
+            # group tokens by category
+            if res['category'] not in data:
+                data[res['category']] = [res['tokens']]
+            else:
+                data[res['category']].append(res['tokens'])
 
 print(data)
