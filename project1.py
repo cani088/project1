34d07238cbca84ad58683374976130cbd002b343
Besnik Cani <besnik.cani@ait.ac.at>
2023-05-03 13:19:46 +0200

    added second step

--- project1.py
+++ project1.py
@@ -8,12 +8,14 @@ from mrjob.step import MRStep
 
 
 class MRWordFrequencyCount(MRJob):
+    categories_tokens = {}
 
     def map_words_categories(self, _, line):
         for review in line.splitlines():
             review = json.loads(review)
             tokens = re.findall(r'\b[^\d\W]+\b|[()[]{}.!?,;:+=-_`~#@&*%€$§\/]^', review["reviewText"])
             # tokens = re.findall(r'\b\w+\b|[(){}\[\].!?,;:+=\-_"\'`~#@&*%€$§\\/]+', review['reviewText'])
+            self.categories_tokens[review['category']] = {}
             tokens = list(set([token.lower() for token in tokens]))
             # self.writeToNewDevset(review['category'], tokens)
             for token in tokens:
@@ -22,11 +24,15 @@ class MRWordFrequencyCount(MRJob):
     def reducer_count_words(self, word, counts):
         yield word, sum(counts)
 
+    def mapper_set_categories_tokens(self, split, count):
+        # from the first step's output, we construct a dictinary that we are going to use for calculating chi values
+        self.categories_tokens[split[0]].__setitem__(split[1], count)
 
     def steps(self):
         return [
             MRStep(mapper=self.map_words_categories,
-                   reducer=self.reducer_count_words)
+                   reducer=self.reducer_count_words),
+            MRStep(mapper=self.mapper_set_categories_tokens)
         ]
 
 if __name__ == '__main__':
