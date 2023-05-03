684bb4a020434ed066dbc1b4b65fef6af7ebd386
Besnik Cani <besnik.cani@ait.ac.at>
2023-05-03 10:49:50 +0200

    sort categories in output

--- project1.py
+++ project1.py
@@ -678,6 +678,7 @@ class MRWordFrequencyCount(MRJob):
         "Health_and_Personal_Care": "22",
         'category_count': "23"
     }
+
     categories_keys_counter = 0
 
     def tokenizeReview(self, review):
@@ -745,7 +746,7 @@ class MRWordFrequencyCount(MRJob):
             self.categories_chi[category].sort(key=lambda x: x['chi'], reverse=True)
             if len(self.categories_chi[category]) > 76:
                 self.categories_chi[category] = self.categories_chi[category][0:75]
-            self.categories_chi[category].sort(key=lambda x: x['token'])
+        dict(sorted(self.categories_chi.items()))
 
 
     def map_words_categories(self, _, line):
