ae35487511c13619ac70cebb3de3fe4ffe8cc12c
Besnik Cani <besnik.cani@ait.ac.at>
2023-05-03 21:53:22 +0200

    asdas

--- project1.py
+++ project1.py
@@ -8,8 +8,6 @@ from mrjob.step import MRStep
 
 
 class MRWordFrequencyCount(MRJob):
-    tokens = []
-    categories_tokens = {'categories_count': []}
     categories_counts = {
         "Apps_for_Android":	2638,
         "Automotive":	1374,
@@ -35,6 +33,32 @@ class MRWordFrequencyCount(MRJob):
         "Toys_and_Game":	2253,
     }
 
+    categories_counts = {
+        "Apps_for_Android":	{},
+        "Automotive":	{},
+        "Baby":	{},
+        "Beauty":	{},
+        "Book":	{},
+        "CDs_and_Vinyl":	{},
+        "Cell_Phones_and_Accessorie":	{},
+        "Clothing_Shoes_and_Jewelry":	{},
+        "Digital_Music":	{},
+        "Electronic":	{},
+        "Grocery_and_Gourmet_Food":	{},
+        "Health_and_Personal_Care":	{},
+        "Home_and_Kitche":	{},
+        "Kindle_Store":	{},
+        "Movies_and_TV":	{},
+        "Musical_Instrument":	{},
+        "Office_Product":	{},
+        "Patio_Lawn_and_Garde":	{},
+        "Pet_Supplie":	{},
+        "Sports_and_Outdoor":	{},
+        "Tools_and_Home_Improvement":	{},
+        "Toys_and_Game":	{},
+        "categories_count": {}
+    }
+
 
     def map_get_categories(self, _, line):
         for review in line.splitlines():
@@ -50,7 +74,6 @@ class MRWordFrequencyCount(MRJob):
             tokens = re.findall(r'\b[^\d\W]+\b|[()[]{}.!?,;:+=-_`~#@&*%€$§\/]^', review["reviewText"])
             # tokens = re.findall(r'\b\w+\b|[(){}\[\].!?,;:+=\-_"\'`~#@&*%€$§\\/]+', review['reviewText'])
             tokens = list(set([token.lower() for token in tokens if len(token) > 2]))
-            self.categories_tokens[review['category']] = {}
             for token in tokens:
                 yield (review['category'], token), 1
             
@@ -58,7 +81,8 @@ class MRWordFrequencyCount(MRJob):
 
     def reducer_count_words(self, word, counts):
         totalCounts = sum(counts)
-        self.tokens.append({'category': word[0], 'token': word[1], 'count': totalCounts})
+        self.categories_tokens[word[0]][word[1]] = totalCounts
+
         yield word, totalCounts
 
 
@@ -75,36 +99,34 @@ class MRWordFrequencyCount(MRJob):
         # D- number of documents not in c without t - N minus total documents in c minus calculated B of each category
         # the formula for calculating chi-squared is
         # N(AD - BC)^2 / (A+B)(A+C)(B+D)(C+D)
-        for token in self.tokens:
-            self.categories_tokens[token['category']][token['token']] = token['count']
 
-        # N = 0
+        N = 0
 
-        # for c in self.categories_tokens['category_count']:
-        #     N += self.categories_tokens['category_count'][c]
-        #     self.categories_counts[c] = self.categories_tokens['category_count'][c]
+        for c in self.categories_tokens['category_count']:
+            N += self.categories_tokens['category_count'][c]
+            self.categories_counts[c] = self.categories_tokens['category_count'][c]
 
-        # # remove category_count
-        # self.categories_tokens.pop('category_count')
+        # remove category_count
+        self.categories_tokens.pop('category_count')
 
-        # for category in self.categories_tokens:
-        #     self.categories_chi[category] = []
-        #     for token in self.categories_tokens[category]:
-        #         A = self.categories_tokens[category][token]
-        #         B = 0
+        for category in self.categories_tokens:
+            self.categories_chi[category] = []
+            for token in self.categories_tokens[category]:
+                A = self.categories_tokens[category][token]
+                B = 0
 
-        #         for c in self.categories_tokens:
-        #             if c == category:
-        #                 continue
-        #             if token in self.categories_tokens[c]:
-        #                 B += self.categories_tokens[c][token]
+                for c in self.categories_tokens:
+                    if c == category:
+                        continue
+                    if token in self.categories_tokens[c]:
+                        B += self.categories_tokens[c][token]
 
-        #         C: int = self.categories_counts[category] - A
-        #         D: int = N - self.categories_counts[category] - B
+                C: int = self.categories_counts[category] - A
+                D: int = N - self.categories_counts[category] - B
 
-        #         # R = N(AD - BC)^2 / (A+B)(A+C)(B+D)(C+D)
-        #         R: float = (N * (((A * D) - (B * C)) ** 2)) / ((A + B) * (A + C) * (B + D) * (C + D))
-        #         self.categories_chi[category].append({"token": token, "chi": R})
+                # R = N(AD - BC)^2 / (A+B)(A+C)(B+D)(C+D)
+                R: float = (N * (((A * D) - (B * C)) ** 2)) / ((A + B) * (A + C) * (B + D) * (C + D))
+                self.categories_chi[category].append({"token": token, "chi": R})
 
 
     def steps(self):
