1cad91ab5c4b1409029e786a4043cbc84c036756
Besnik Cani <besnik.cani@ait.ac.at>
2023-05-03 13:37:01 +0200

    set categories counts as static dictionary

--- project1.py
+++ project1.py
@@ -10,6 +10,39 @@ from mrjob.step import MRStep
 class MRWordFrequencyCount(MRJob):
     categories_tokens = {'category_count': {}}
 
+    categories_counts = {
+        "Apps_for_Android":	2638,
+        "Automotive":	1374,
+        "Baby":	916,
+        "Beauty":	2023,
+        "Book":	22507,
+        "CDs_and_Vinyl":	3749,
+        "Cell_Phones_and_Accessorie":	3447,
+        "Clothing_Shoes_and_Jewelry":	5749,
+        "Digital_Music":	836,
+        "Electronic":	7825,
+        "Grocery_and_Gourmet_Food":	1297,
+        "Health_and_Personal_Care":	2982,
+        "Home_and_Kitche":	4254,
+        "Kindle_Store":	3205,
+        "Movies_and_TV":	4607,
+        "Musical_Instrument":	500,
+        "Office_Product":	1243,
+        "Patio_Lawn_and_Garde":	994,
+        "Pet_Supplie":	1235,
+        "Sports_and_Outdoor":	3269,
+        "Tools_and_Home_Improvement":	1926,
+        "Toys_and_Game":	2253,
+    }
+
+    def map_get_categories(self, _, line):
+        for review in line.splitlines():
+            review = json.loads(review)
+            yield review['category'], 1
+
+    # def reducer_count_categories(self):
+
+
     def map_words_categories(self, _, line):
         for review in line.splitlines():
             review = json.loads(review)
@@ -20,7 +53,6 @@ class MRWordFrequencyCount(MRJob):
             # self.writeToNewDevset(review['category'], tokens)
             for token in tokens:
                 yield (review['category'], token), 1
-            yield ('category_count', review['category']), 1
           
     def reducer_count_words(self, word, counts):
         yield word, sum(counts)
@@ -31,6 +63,10 @@ class MRWordFrequencyCount(MRJob):
 
     def steps(self):
         return [
+            # MRStep(mapper=self.map_get_categories,
+            #        reducer=self.reducer_count_words,
+            #     #    reducer_final=self.reducer_count_categories
+            #        ),
             MRStep(mapper=self.map_words_categories,
                    reducer=self.reducer_count_words),
             MRStep(mapper=self.mapper_set_categories_tokens)
