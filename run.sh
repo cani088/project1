5cf4986021be9cf874d523670ef24eb50aebb2a6
Besnik Cani <besnik.cani@ait.ac.at>
2023-05-04 17:30:07 +0200

    updated script

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 project1.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.4.jar -r  $1 > output2.txt
+python3 project1.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.4.jar -r hadoop $1 > output2.txt
